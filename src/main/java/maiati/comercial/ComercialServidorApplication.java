03e347b0af46a2c1cfe43d548903c3b1934fdfe5
Dowglas Maia <dowglasmaia@gmail.com>
2019-01-22 22:24:01 -0200

    Criando Projeto

---
+++ src/main/java/maiati/comercial/ComercialServidorApplication.java
@@ -0,0 +1,14 @@
+package maiati.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercialServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercialServidorApplication.class, args);
+	}
+
+}
+
