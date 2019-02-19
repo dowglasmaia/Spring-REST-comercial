7bc448b955312fedddf958450e03d070f90eef64
Dowglas Maia <dowglasmaia@gmail.com>
2019-02-19 16:00:16 -0300

    Implementando CORS para o Sistema

--- src/main/java/maiati/comercial/securityconfig/SecurityConfig.java
+++ src/main/java/maiati/comercial/securityconfig/SecurityConfig.java
@@ -1,18 +1,27 @@
 package maiati.comercial.securityconfig;
 
+import java.util.Arrays;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -20,9 +29,11 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
 		http.csrf().disable().authorizeRequests()  /* Usar o .csrf().disable() - somente em Ambiente de Desenvolvimento  */
 		.antMatchers("/login").permitAll()
 		.antMatchers("/error").permitAll()
+		.antMatchers("/cargos/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		
@@ -31,6 +42,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		
 		/*filtragem das requisições - passando pela class JWTFilter */
 		.addFilterBefore(new JWTFilter(), UsernamePasswordAuthenticationFilter.class);	
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	
 	}	
 	
@@ -42,6 +54,17 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.usersByUsernameQuery("SELECT login, senha, 1 as enabled FROM usuario WHERE login=?")
 		.authoritiesByUsernameQuery("SELECT login, 'ROLE USER' FROM usuario WHERE login=?") 
 		.passwordEncoder(new BCryptPasswordEncoder()); // faz a criptografia da senha do Usuairo
+	}
+	
+	/*Configuração do Cors da Aplicação*/
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS")); //Liberando o Curs de Forma esplicita para os Metdos HTTP da lista,
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+
 	}
 
 }
